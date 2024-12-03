7d917b067ffce2f3f7f3f3b9771cda1c12e08f44
Nadira Maries <maries.lavinia@gmail.com>
2024-12-03 14:55:29 +0200

    lab5

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace MariesNadiraLab4
+﻿using System;
+using MariesNadiraLab4.Data;
+using System.IO;
+
+namespace MariesNadiraLab4
 {
+
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
